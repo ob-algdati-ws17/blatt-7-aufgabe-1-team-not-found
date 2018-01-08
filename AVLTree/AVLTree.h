a7c5ce169a5ded4ac74975f6b6a6044318439cf0
Robert Altvatter <raltvatt@hm.edu>
2018-01-08 09:28:02 +0100

    header File.

--- AVLTree/AVLTree.h
+++ AVLTree/AVLTree.h
@@ -5,4 +5,48 @@
 #ifndef BLATT_7_AUFGABE_1_TEAM_NOT_FOUND_AVLTREE_H
 #define BLATT_7_AUFGABE_1_TEAM_NOT_FOUND_AVLTREE_H
 
+
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class AVLTree{
+private:
+    struct Node{
+        //attributes.
+        int balance = 0;
+        const int key;
+        Node *left = nullptr;
+        Node *right = nullptr;
+        Node *prev = nullptr;
+
+        //Konstruktoren
+        Node(int);
+        Node(int, Node *prev);
+        Node(int, Node *prev, Node *left, Node *right);
+        ~Node();
+        //Methoden
+        bool search(int);
+        void rotateLeft();
+        void rotateRight();
+        void rotateLeftRight();
+        void rotateRightLeft();
+        void upin();
+        void upout();
+        bool insert(int);
+        bool remove(int);
+
+    };
+    Node *root = nullptr;
+
+public:
+    ~AVLTree();
+    bool search(int);
+    void insert(int);
+    void remove(int);
+    bool isEmpty();
+};
+
 #endif //BLATT_7_AUFGABE_1_TEAM_NOT_FOUND_AVLTREE_H
